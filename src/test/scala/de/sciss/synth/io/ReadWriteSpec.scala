b7a7f1d0a651ab957ebf59074fab17984bfd8f7e
Sciss <contact@sciss.de>
2012-12-10 02:01:34 +0100

    passes buffer contents test

--- src/test/scala/de/sciss/synth/io/ReadWriteSpec.scala
+++ src/test/scala/de/sciss/synth/io/ReadWriteSpec.scala
@@ -27,7 +27,35 @@ class ReadWriteSpec extends fixture.FlatSpec with ShouldMatchers {
 
    val bufSize    = 8192
    val totalSize  = 10000
+   val size2      = totalSize - bufSize
    val sr         = 44100.0
+
+   def generate( buf: Frames, num: Int, seed: Long = 0L ) {
+      val rnd = new util.Random( seed )
+      var ch = 0; while( ch < buf.length ) {
+         val cb = buf( ch )
+         var i = 0; while( i < num ) {
+            cb( i ) = rnd.nextFloat()
+         i += 1 }
+      ch += 1 }
+   }
+
+   def compare( a: Frames, b: Frames, num: Int, fmt: SampleFormat ) {
+      val tol = fmt match {
+         case SampleFormat.Float    => 0.0
+         case SampleFormat.Double   => 0.0
+         case _ => 2.02 / (1L << fmt.bitsPerSample)   // whatever... ?
+      }
+      assert( a.length === b.length )
+      var ch = 0; while( ch < a.length ) {
+         val ca = a( ch )
+         val cb = b( ch )
+         var i = 0; while( i < num ) {
+            val diff = math.abs( ca( i ) - cb( i ))
+            assert( diff <= tol, "Buffer content differs (err = " + diff + ") for " + fmt )
+         i += 1 }
+      ch += 1 }
+   }
 
    rwTypes.foreach { typ =>
       typ.supportedFormats.foreach { smpFmt =>
@@ -39,9 +67,11 @@ class ReadWriteSpec extends fixture.FlatSpec with ShouldMatchers {
                assert( afOut.isReadable )
                assert( afOut.isWritable )
                assert( afOut.position        === 0L )
-               val buf = afOut.buffer( bufSize )
-               afOut.write( buf )
-               afOut.write( buf, 0, totalSize - bufSize )
+               val bufOut = afOut.buffer( bufSize )
+               generate( bufOut, bufSize, 0L )
+               afOut.write( bufOut )
+               generate( bufOut, size2, 1L )
+               afOut.write( bufOut, 0, size2 )
                assert( afOut.position        === totalSize.toLong )
                val framesWritten = afOut.numFrames
                assert( framesWritten         === afOut.spec.numFrames )
@@ -49,6 +79,7 @@ class ReadWriteSpec extends fixture.FlatSpec with ShouldMatchers {
                assert( !afOut.isOpen )
 
                val afIn = AudioFile.openRead( f )
+               val bufIn = afIn.buffer( bufSize )
                assert( afIn.isOpen     )
                assert( afIn.isReadable )
                assert( !afIn.isWritable )
@@ -60,8 +91,12 @@ class ReadWriteSpec extends fixture.FlatSpec with ShouldMatchers {
                assert( afIn.sampleRate       === sr )
                assert( afIn.file             === Some( f ))
                assert( afIn.fileType         === typ )
-               afIn.read( buf )
-               afIn.read( buf, 0, totalSize - bufSize )
+               afIn.read( bufIn )
+               generate( bufOut, bufSize, 0L )
+               compare( bufIn, bufOut, bufSize, smpFmt )
+               afIn.read( bufIn, 0, size2 )
+               generate( bufOut, size2, 1L )
+               compare( bufIn, bufOut, size2, smpFmt )
                assert( afIn.position         === totalSize.toLong )
                afIn.close()
                assert( !afIn.isOpen )
